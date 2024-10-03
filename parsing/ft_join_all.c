2145cc4816930727b54f61a2e0f588ad298b0f73
Vincent Spinosa <vincentspinosa@MBP-de-Vincent.lan>
2024-10-03 03:13:20 +0200

    free()s and protections

--- parsing/ft_join_all.c
+++ parsing/ft_join_all.c
@@ -6,7 +6,7 @@
 /*   By: vispinos <vispinos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 15:59:55 by vispinos          #+#    #+#             */
-/*   Updated: 2024/10/03 02:46:14 by vispinos         ###   ########.fr       */
+/*   Updated: 2024/10/03 03:11:32 by vispinos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,13 @@ char	*ft_join_all(char *line, char *var_value, int i_save, int cut)
 
 	temp_str = ft_substr(line, 0, i_save);
 	pre_line = ft_strjoin(temp_str, var_value);
+	if (temp_str)
+		free(temp_str);
 	temp_str = ft_strdup(&line[i_save + cut]);
 	new_line = ft_strjoin(pre_line, temp_str);
+	if (pre_line)
+		free(pre_line);
+	if (temp_str)
+		free(temp_str);
 	return (new_line);
 }
