d7a677418e9b3096a3a8c9d4379b35f91cc6c4f9
Vincent Spinosa <92981607+vincentspinosa@users.noreply.github.com>
2024-10-04 23:30:14 +0000

    indication when functions are used

--- parsing/libft/libft.h
+++ parsing/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: vispinos <vispinos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 12:55:42 by vispinos          #+#    #+#             */
-/*   Updated: 2024/10/03 02:35:31 by vispinos         ###   ########.fr       */
+/*   Updated: 2024/10/04 23:27:17 by vispinos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,25 @@
 
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd); // used
 void	ft_putnbr_fd(int n, int fd);
 
-size_t	ft_strlen(const char *s);
+size_t	ft_strlen(const char *s); // used
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strdup(const char *s1);
+int		ft_strncmp(const char *s1, const char *s2, size_t n); // used
+char	*ft_strdup(const char *s1); // used
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2); // used
+char	*ft_substr(char const *s, unsigned int start, size_t len); // used
 char	*ft_strtrim(char const *s1, char const *set);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize); // used
 
 int		ft_atoi(const char *str);
-char	*ft_itoa(int n);
+char	*ft_itoa(int n); // used
 
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
@@ -47,8 +47,8 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_calloc(size_t count, size_t size);
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
+int		ft_isalpha(int c); // used
+int		ft_isdigit(int c); // used
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
